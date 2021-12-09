6f05e5df8f24187beb16b864cab7d6b24f690f67
anonymous anonymous <cat.knoerr@kellogg.northwestern.edu>
2021-12-09 19:00:43 +0000

    Generate graphiti resources for players

---
+++ app/controllers/api/v1/players_controller.rb
@@ -0,0 +1,41 @@
+class Api::V1::PlayersController < Api::V1::GraphitiController
+  def index
+    players = PlayerResource.all(params)
+    respond_with(players)
+  end
+
+  def show
+    player = PlayerResource.find(params)
+    respond_with(player)
+  end
+
+  def create
+    player = PlayerResource.build(params)
+
+    if player.save
+      render jsonapi: player, status: 201
+    else
+      render jsonapi_errors: player
+    end
+  end
+
+  def update
+    player = PlayerResource.find(params)
+
+    if player.update_attributes
+      render jsonapi: player
+    else
+      render jsonapi_errors: player
+    end
+  end
+
+  def destroy
+    player = PlayerResource.find(params)
+
+    if player.destroy
+      render jsonapi: { meta: {} }, status: 200
+    else
+      render jsonapi_errors: player
+    end
+  end
+end
---
+++ app/resources/player_resource.rb
@@ -0,0 +1,17 @@
+class PlayerResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :username, :string
+  attribute :photo, :string
+  attribute :location, :string
+  attribute :age, :integer
+  attribute :description, :string
+  attribute :preferences, :string
+  attribute :characters, :string
+
+  # Direct associations
+
+  # Indirect associations
+
+end
