03814375b29a2d862783b1968cf8acfaa49c3895
anonymous anonymous <cat.knoerr@kellogg.northwestern.edu>
2021-12-09 18:59:20 +0000

    Whitelist columns in Player for ActiveAdmin

--- app/admin/players.rb
+++ app/admin/players.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Player do
 
+ permit_params :username, :photo, :location, :age, :description, :preferences, :characters
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
