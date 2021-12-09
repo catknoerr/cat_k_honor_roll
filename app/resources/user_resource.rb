cf503ce1dc0cfe3b1c6489ce2bbdf0e9fbeef110
anonymous anonymous <cat.knoerr@kellogg.northwestern.edu>
2021-12-09 19:00:35 +0000

    Generate graphiti resources for users

---
+++ app/resources/user_resource.rb
@@ -0,0 +1,18 @@
+class UserResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :email, :string
+  attribute :password, :string
+  attribute :location, :string
+  attribute :username, :string
+  attribute :first_name, :string
+  attribute :last_name, :string
+  attribute :dm_id, :integer
+  attribute :player_id, :integer
+
+  # Direct associations
+
+  # Indirect associations
+
+end
