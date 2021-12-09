4ada274fa0fa092bda5558c71a75ef3a1f0e8609
anonymous anonymous <cat.knoerr@kellogg.northwestern.edu>
2021-12-09 19:00:45 +0000

    Generate graphiti resources for games

---
+++ app/resources/game_resource.rb
@@ -0,0 +1,16 @@
+class GameResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :dm_id, :integer
+  attribute :player_id, :integer
+  attribute :date, :date
+  attribute :time, :string
+  attribute :capacity, :integer
+  attribute :location, :string
+
+  # Direct associations
+
+  # Indirect associations
+
+end
